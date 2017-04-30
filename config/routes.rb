ffb229e6dc8f64cc4934abdd378b51d0c7ce0b10
Jeremy Kirkham <jeremy@streame.com.au>
2017-04-30 16:44:23 +1000

    Static layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/contact-us', to: 'static_pages#contact'
+  get '/about-us', to: 'static_pages#about'
+  get '/hello', to: 'static_pages#helloworld'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
