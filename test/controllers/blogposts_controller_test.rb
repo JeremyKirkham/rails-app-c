ba6c86ccde331f824eee74255b55531e43497319
Jeremy Kirkham <jeremy@streame.com.au>
2017-05-15 17:30:14 +1000

    wip on tests

--- test/controllers/blogposts_controller_test.rb
+++ test/controllers/blogposts_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class BlogpostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get blogposts_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get blogposts_create_url
-    assert_response :success
+  def setup
+    @user = users(:one)
+    log_in_as(@user)
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,22 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'MyString')
+    post login_path, params: { session: { email: user.email, password: password } }
+  end
 end
